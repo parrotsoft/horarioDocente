f9c2727b63ecfd6bddce1137d924483808c6ad7b
Miguel Lopez Ariza <lopezarizamiguel@gmail.com>
2020-05-17 14:32:48 -0500

    cambios locales

---
+++ test/connection/ConexionTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author miguel
+ */
+public class ConexionTest {
+    
+    public ConexionTest() {
+    }
+
+    @org.junit.Test
+    public void testConectart() throws Exception {
+    }
+    
+}
